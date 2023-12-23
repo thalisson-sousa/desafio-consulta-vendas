8a4425ff9ea2700af292f3f4b35d8ff8e4b63adf
Thalisson <thalissonpop91@gmail.com>
2023-12-23 18:55:44 -0300

    create SaleService

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,16 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
+import com.devsuperior.dsmeta.dto.SaleDTO;
+import com.devsuperior.dsmeta.dto.SaleSumAmountDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -19,5 +27,75 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleDTO> findByDate(String minDate, String maxDate, String name, Pageable pageable) {
+
+		if (maxDate.isEmpty() && minDate.isEmpty()) {
+			Page<Sale> result = repository.report( pageable);
+			return result.map(x -> new SaleDTO(x));
+		} else {
+
+			if (name == null) {
+				name = "";
+			}
+
+			LocalDate initialDate;
+			LocalDate finishDate;
+
+			if (maxDate.isEmpty()) {
+				finishDate = transformMaxdate();
+			} else {
+				finishDate = LocalDate.parse(maxDate);
+			}
+
+			if (minDate.isEmpty()) {
+				initialDate = transformMindate(finishDate, maxDate);
+			} else {
+				initialDate = LocalDate.parse(minDate);
+			}
+
+			Page<Sale> result = repository.reportForDate(initialDate, finishDate, name, pageable);
+			return result.map(x -> new SaleDTO(x));
+		}
+	}
+
+	public Page<SaleSumAmountDTO> searchSumAmount(String minDate, String maxDate, Pageable pageable) {
+
+		if (maxDate.isEmpty() && minDate.isEmpty()) {
+			Page<SaleSumAmountDTO> result = repository.summarySum(pageable);
+			return result;
+		} else {
+			LocalDate initialDate;
+			LocalDate finishDate;
+
+			if (maxDate.isEmpty()) {
+				finishDate = transformMaxdate();
+			} else {
+				finishDate = LocalDate.parse(maxDate);
+			}
+
+			if (minDate.isEmpty()) {
+				initialDate = transformMindate(finishDate, maxDate);
+			} else {
+				initialDate = LocalDate.parse(minDate);
+			}
+
+			Page<SaleSumAmountDTO> result = repository.summarySumForDate(initialDate, finishDate, pageable);
+			return result;
+		}
+	}
+
+	public LocalDate transformMaxdate() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		return today;
+	}
+
+	public LocalDate transformMindate(LocalDate finishDate, String maxDate) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		finishDate = LocalDate.parse(maxDate, formatter);
+		LocalDate result = finishDate.minusYears(1L);
+		return result;
 	}
 }
